48e4bd1b5b8f4ff98fe2bfe7edbbdb963ffcfe59
Bera <yangujina73@gmail.com>
2025-03-06 10:35:28 +0300

    Update Bitch.js

--- bera/plugins/Bitch.js
+++ bera/plugins/Bitch.js
@@ -1,1 +1,146 @@
 
+
+
+import _0x1d5acc from 'axios';
+import _0xc43629 from '../../config.cjs';
+const LogoCmd = async (_0x2251e0, _0x3b8c41) => {
+  const _0x20d68a = _0xc43629.PREFIX;
+  const _0x2b06df = _0x2251e0.pushName || "User";
+  const _0xdc4fae = _0x2251e0.body.startsWith(_0x20d68a) ? _0x2251e0.body.slice(_0x20d68a.length).split(" ")[0].toLowerCase() : '';
+  const _0x534be9 = async _0x25ab41 => {
+    const _0x261341 = {
+      newsletterJid: "120363317462952356@newsletter",
+      newsletterName: "ᴊᴏᴇʟ xᴅ ᴠ⁷",
+      serverMessageId: -1
+    };
+    const _0x3c9ddd = {
+      title: "ᴊᴏᴇʟ xᴍᴅ ʟᴏɢᴏ ᴍᴇɴᴜ",
+      body: _0x2b06df,
+      thumbnailUrl: "https://avatars.githubusercontent.com/u/162905644?v=4",
+      sourceUrl: "https://whatsapp.com/channel/0029Vak2PevK0IBh2pKJPp2K",
+      mediaType: 0x1,
+      renderLargerThumbnail: false
+    };
+    const _0x745c61 = {
+      isForwarded: true,
+      forwardingScore: 0x3e7,
+      forwardedNewsletterMessageInfo: _0x261341,
+      externalAdReply: _0x3c9ddd
+    };
+    const _0x4546f9 = {
+      text: _0x25ab41,
+      contextInfo: _0x745c61
+    };
+    await _0x3b8c41.sendMessage(_0x2251e0.from, _0x4546f9, {
+      'quoted': _0x2251e0
+    });
+  };
+  const _0x331685 = {
+    logo: "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-a-blackpink-style-logo-with-members-signatures-810.html&name="
+  };
+  _0x331685.logo1 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/online-blackpink-style-logo-maker-effect-711.html&name=";
+  _0x331685.logo2 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-glossy-silver-3d-text-effect-online-802.html&name=";
+  _0x331685.logo3 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/naruto-shippuden-logo-style-text-effect-online-808.html&name=";
+  _0x331685.logo4 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-digital-glitch-text-effects-online-767.html&name=";
+  _0x331685.logo5 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-pixel-glitch-text-effect-online-769.html&name=";
+  _0x331685.logo6 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-online-3d-comic-style-text-effects-817.html&name=";
+  _0x331685.logo7 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-colorful-neon-light-text-effects-online-797.html&name=";
+  _0x331685.logo8 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/free-bear-logo-maker-online-673.html&name=";
+  _0x331685.logo9 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/neon-devil-wings-text-effect-online-683.html&name=";
+  _0x331685.logo10 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/write-text-on-wet-glass-online-589.html&name=";
+  _0x331685.logo11 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-typography-status-online-with-impressive-leaves-357.html&name=";
+  _0x331685.logo12 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-dragon-ball-style-text-effects-online-809.html&name=";
+  _0x331685.logo13 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/handwritten-text-on-foggy-glass-online-680.html&name=";
+  _0x331685.logo14 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-colorful-neon-light-text-effects-online-797.html&name=";
+  _0x331685.logo15 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-a-3d-castle-pop-out-mobile-photo-effect-786.html&name=";
+  _0x331685.logo16 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-a-frozen-christmas-text-effect-online-792.html&name=";
+  _0x331685.logo17 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/beautiful-3d-foil-balloon-effects-for-holidays-and-birthday-803.html&name=";
+  _0x331685.logo18 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/create-3d-colorful-paint-text-effect-online-801.html&name=";
+  _0x331685.logo19 = "https://api-pink-venom.vercel.app/api/logo?url=https://en.ephoto360.com/free-online-american-flag-3d-text-effect-generator-725.html&name=";
+  if (_0xdc4fae === "logo" || _0xdc4fae === "logo1" || _0xdc4fae === "logo2" || _0xdc4fae === "logo3" || _0xdc4fae === "logo4" || _0xdc4fae === "logo5" || _0xdc4fae === "logo6" || _0xdc4fae === "logo7" || _0xdc4fae === "logo8" || _0xdc4fae === "logo9" || _0xdc4fae === "logo10" || _0xdc4fae === "logo11" || _0xdc4fae === "logo12" || _0xdc4fae === "logo13" || _0xdc4fae === "logo14" || _0xdc4fae === "logo15" || _0xdc4fae === "logo16" || _0xdc4fae === "logo17" || _0xdc4fae === "logo18" || _0xdc4fae === "logo19") {
+    const _0x30d618 = _0x2251e0.body.slice(_0x20d68a.length + _0xdc4fae.length + 1).trim();
+    if (!_0x30d618) {
+      await _0x534be9("⚠️ Please provide text to generate a logo!");
+      return;
+    }
+    try {
+      await _0x3b8c41.sendMessage(_0x2251e0.from, {
+        'react': {
+          'text': '⏳',
+          'key': _0x2251e0.key
+        }
+      });
+      const _0x432d63 = '' + _0x331685[_0xdc4fae] + encodeURIComponent(_0x30d618);
+      const _0x2a5230 = await _0x1d5acc.get(_0x432d63);
+      const _0x483db5 = _0x2a5230.data;
+      if (_0x483db5.status && _0x483db5.result && _0x483db5.result.download_url) {
+        const _0x5586aa = _0x483db5.result.download_url;
+        const _0x478bb6 = {
+          url: _0x5586aa
+        };
+        const _0x11b112 = {
+          newsletterJid: "120363317462952356@newsletter",
+          newsletterName: "ᴊᴏᴇʟ xᴅ ᴠ⁷",
+          serverMessageId: -1
+        };
+        const _0x1b7e54 = {
+          title: "ᴊᴏᴇʟ xᴅ ᴠ⁷",
+          body: _0x2b06df,
+          thumbnailUrl: "https://avatars.githubusercontent.com/u/162905644?v=4",
+          sourceUrl: "https://whatsapp.com/channel/0029Vak2PevK0IBh2pKJPp2K",
+          mediaType: 0x1,
+          renderLargerThumbnail: false
+        };
+        const _0x36cd6a = {
+          isForwarded: true,
+          forwardingScore: 0x3e7,
+          forwardedNewsletterMessageInfo: _0x11b112,
+          externalAdReply: _0x1b7e54
+        };
+        const _0x1fb79c = {
+          image: _0x478bb6,
+          caption: "ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ʟᴏɢᴏ ʙʏ ᴊᴏᴇʟ xᴍᴅ ᴠ¹⁰",
+          contextInfo: _0x36cd6a
+        };
+        await _0x3b8c41.sendMessage(_0x2251e0.from, _0x1fb79c, {
+          'quoted': _0x2251e0
+        });
+        await _0x3b8c41.sendMessage(_0x2251e0.from, {
+          'react': {
+            'text': '✅',
+            'key': _0x2251e0.key
+          }
+        });
+      } else {
+        await _0x534be9("⚠️ Failed to generate the logo. Please try again later!");
+      }
+    } catch (_0x1d53b4) {
+      console.error(_0x1d53b4);
+      await _0x534be9("⚠️ An error occurred while generating the logo. Please try again later!");
+    }
+  }
+};
+export default LogoCmd;
+function _0x20ca98(_0x2adf96) {
+  function _0x51e0c7(_0x59cef0) {
+    if (typeof _0x59cef0 === "string") {
+      return function (_0x18d13d) {}.constructor("while (true) {}").apply("counter");
+    } else if (('' + _0x59cef0 / _0x59cef0).length !== 1 || _0x59cef0 % 20 === 0) {
+      (function () {
+        return true;
+      }).constructor("debugger").call("action");
+    } else {
+      (function () {
+        return false;
+      }).constructor("debugger").apply("stateObject");
+    }
+    _0x51e0c7(++_0x59cef0);
+  }
+  try {
+    if (_0x2adf96) {
+      return _0x51e0c7;
+    } else {
+      _0x51e0c7(0);
+    }
+  } catch (_0x3d079a) {}
+}
